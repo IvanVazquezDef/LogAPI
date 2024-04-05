0181e362867430335f2c3fd0a3e85ae6d0ed36c4
Ivan Vazquez <Ivan.Vazquez@definityfirst.com>
2024-04-05 11:52:40 -0600

    Add project files.

---
+++ LogAPI/Model/Log.cs
@@ -0,0 +1,19 @@
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+
+namespace LogAPI.Models
+{
+    public class Log
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime Timestamp { get; set; }
+        public string UserId { get; set; }
+        public string PageId { get; set; }
+
+        public Log() { }
+    }
+}
---
+++ LogAPI/Repositories/ILogCollection.cs
@@ -0,0 +1,21 @@
+using LogAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LogAPI.Repositories
+{
+    public interface ILogCollection
+    {
+        Task InsertLog(Log log);
+        Task UpdateLog(Log log);
+        Task DeleteLog(string id);
+
+        Task<List<Log>> GetAllLogs();
+        Task<List<Log>> GetLogsByDate(DateTime date);
+        Task<List<Log>> GetLogsByPageId(string pageId);
+        Task<List<Log>> GetLogsByUserId(string userId);
+        Task<List<string>> GetUniqueViewedPagesByDateAndUser(string userId, DateTime startDate, DateTime endDate);
+        Task<List<string>> GetLoyalCustomers(DateTime dateOne, DateTime dateTwo);
+    }
+}
